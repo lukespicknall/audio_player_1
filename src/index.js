6a05f2f4c8a40c8beb5acb01e16bc6b454b8dccd
Luke Spicknall <lukejspicknall@gmail.com>
2024-02-14 21:13:09 -0500

    add wavesurfer.js with basic import and audio waveform creation.

--- src/index.js
+++ src/index.js
@@ -1,47 +1,61 @@
 import './style.css';
+import WaveSurfer from 'wavesurfer.js';
 
-const audio = document.getElementById("audio");
-const playPauseButton = document.getElementById("play-pause-button");
-const volumeControl = document.getElementById("volume-control");
-const progressBar = document.getElementById("progress-bar");
-const currentTimeDisplay = document.getElementById("current-time");
-const totalTimeDisplay = document.getElementById("total-time");
-
-let isPlaying = false;
-
-playPauseButton.addEventListener("click", () => {
-  if (isPlaying) {
-    audio.pause();
-    playPauseButton.textContent = "Play";
-  } else {
-    audio.play();
-    playPauseButton.textContent = "Pause";
-  }
-  isPlaying = !isPlaying;
+const wavesurfer = WaveSurfer.create({
+  container: document.body,
+  waveColor: 'rgb(200, 0, 200)',
+  progressColor: 'rgb(100, 0, 100)',
+  url: '../src/audio/09_DadSingsKristofferson.mp3',
 });
 
-volumeControl.addEventListener("input", () => {
-  audio.volume = volumeControl.value;
+wavesurfer.on('click', () => {
+  wavesurfer.play();
 });
 
-audio.addEventListener("timeupdate", () => {
-  const currentTime = audio.currentTime;
-  const duration = audio.duration;
+// document.appendChild(wavesurfer);
 
-  const currentMinutes = Math.floor(currentTime / 60);
-  const currentSeconds = Math.floor(currentTime % 60);
-  const totalMinutes = Math.floor(duration / 60);
-  const totalSeconds = Math.floor(duration % 60);
+// const audio = document.getElementById('audio');
+// const playPauseButton = document.getElementById('play-pause-button');
+// const volumeControl = document.getElementById('volume-control');
+// const progressBar = document.getElementById('progress-bar');
+// const currentTimeDisplay = document.getElementById('current-time');
+// const totalTimeDisplay = document.getElementById('total-time');
 
-  currentTimeDisplay.textContent = `${currentMinutes}:${
-    currentSeconds < 10 ? "0" : ""
-  }${currentSeconds}`;
-  totalTimeDisplay.textContent = `${totalMinutes}:${
-    totalSeconds < 10 ? "0" : ""
-  }${totalSeconds}`;
+// let isPlaying = false;
 
-  const progress = (currentTime / duration) * 100;
-  progressBar.style.width = `${progress}%`;
-});
+// playPauseButton.addEventListener('click', () => {
+//   if (isPlaying) {
+//     audio.pause();
+//     playPauseButton.textContent = 'Play';
+//   } else {
+//     audio.play();
+//     playPauseButton.textContent = 'Pause';
+//   }
+//   isPlaying = !isPlaying;
+// });
 
-console.log('hey earth');
+// volumeControl.addEventListener('input', () => {
+//   audio.volume = volumeControl.value;
+// });
+
+// audio.addEventListener('timeupdate', () => {
+//   const { currentTime } = audio;
+//   const { duration } = audio;
+
+//   const currentMinutes = Math.floor(currentTime / 60);
+//   const currentSeconds = Math.floor(currentTime % 60);
+//   const totalMinutes = Math.floor(duration / 60);
+//   const totalSeconds = Math.floor(duration % 60);
+
+//   currentTimeDisplay.textContent = `${currentMinutes}:${
+//     currentSeconds < 10 ? '0' : ''
+//   }${currentSeconds}`;
+//   totalTimeDisplay.textContent = `${totalMinutes}:${
+//     totalSeconds < 10 ? '0' : ''
+//   }${totalSeconds}`;
+
+//   const progress = (currentTime / duration) * 100;
+//   progressBar.style.width = `${progress}%`;
+// });
+
+// console.log('hey earth');
